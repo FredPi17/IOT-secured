802ba40a981da1bdcc5dc5ea21aca227b866cf0a
Frédéric Pinaud <f.pinaud@lancey.fr>
2021-03-29 14:12:03 +0200

    containers networking and grafana dashboard WIP

--- mariadb/schemas/mqtt-user.sql
+++ mariadb/schemas/mqtt-user.sql
@@ -1,3 +1,5 @@
 CREATE USER 'mqtt'@'172.26.0.5' Identified BY 'mqtt-password';
 GRANT ALL privileges ON `mqtt-data`.* TO 'mqtt'@'172.26.0.5';
+CREATE USER 'grafana'@'172.26.0.6' Identified BY 'grafana-password';
+GRANT ALL privileges ON `mqtt-data`.* TO 'grafana'@'172.26.0.6';
 FLUSH PRIVILEGES;
